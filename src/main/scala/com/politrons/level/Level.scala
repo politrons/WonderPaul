5a469b183b3dbeee2cd5c1d2bb450ee1bfd9f306
politrons <politrons@gmail.com>
2022-08-24 18:23:54 +0200

    refactor

--- src/main/scala/com/politrons/level/Level.scala
+++ src/main/scala/com/politrons/level/Level.scala
@@ -17,14 +17,17 @@ class Level extends JFrame {
   implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(Executors.newFixedThreadPool(10))
 
   val thunderboltEngine = new ThunderboltEngine()
+  val characterEngine = new CharacterEngine(thunderboltEngine)
   val gameOverEngine = new GameOverEngine()
   val heart1Engine = new HeartEngine(100, 10)
   val heart2Engine = new HeartEngine(70, 10)
   val heart3Engine = new HeartEngine(40, 10)
-  val enemyEngine1 = new EnemyEngine("Enemy1", 250, 400, enemy1MovePattern,thunderboltEngine)
-  val enemyEngine2 = new EnemyEngine("Enemy1", 700, 400, enemy2MovePattern,thunderboltEngine)
-  val enemyEngine3 = new EnemyEngine("Enemy1", 546, 162, enemy3MovePattern,thunderboltEngine)
-  val characterEngine = new CharacterEngine(thunderboltEngine)
+  val enemyEngine1 = new EnemyEngine("Enemy1", 250, 400,
+    enemy1MovePattern, characterEngine, heart1Engine, heart2Engine, heart3Engine, gameOverEngine, thunderboltEngine)
+  val enemyEngine2 = new EnemyEngine("Enemy1", 700, 400,
+    enemy1MovePattern, characterEngine, heart1Engine, heart2Engine, heart3Engine, gameOverEngine, thunderboltEngine)
+  val enemyEngine3 = new EnemyEngine("Enemy1", 546, 162,
+    enemy1MovePattern, characterEngine, heart1Engine, heart2Engine, heart3Engine, gameOverEngine, thunderboltEngine)
 
   initGame()
 
@@ -45,44 +48,6 @@ class Level extends JFrame {
     setLocationRelativeTo(null)
     setResizable(false)
     setDefaultCloseOperation(3)
-    collisionEngine()
-  }
-
-  /**
-   * As long as enemies are alive, we check constantly if any of the enemies of the map hit the main character
-   */
-  private def collisionEngine() = {
-    Future {
-      val deviation = 10
-      while (enemyEngine1.enemyAlive && enemyEngine2.enemyAlive && enemyEngine3.enemyAlive) {
-        checkCharacterEnemyCollision(deviation, enemyEngine1.enemy)
-        checkCharacterEnemyCollision(deviation, enemyEngine2.enemy)
-        checkCharacterEnemyCollision(deviation, enemyEngine3.enemy)
-        Thread.sleep(100)
-      }
-    }
-  }
-
-  /**
-   * Function to check if the character collision with an enemy.
-   * In case of collision we reduce one heart in the level, and we set
-   * the character like dead.
-   * In case we lose all hearts the game is over.
-   */
-  private def checkCharacterEnemyCollision(deviation: Int, enemy1: Enemy): Unit = {
-    val charX = characterEngine.character.x
-    val charY = characterEngine.character.y
-    val xComp = Math.abs(charX - enemy1.x)
-    val yComp = Math.abs(charY - enemy1.y)
-    if (xComp <= deviation && yComp <= deviation) {
-      characterEngine.live match {
-        case 3 => heart3Engine.removeHeart()
-        case 2 => heart2Engine.removeHeart()
-        case 1 => heart1Engine.removeHeart(); gameOverEngine.setVisible(true)
-      }
-      characterEngine.live -= 1
-      characterEngine.characterDeadAnimation()
-    }
   }
 
 }
